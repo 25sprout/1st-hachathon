f66b676450c695fc5601884e81138f3a81d77761
Eddie Wen <eddiewen.tw@gmail.com>
2017-11-22 11:25:33 +0800

    calculate left time

---
+++ src/js/appConfig.js
@@ -0,0 +1,16 @@
+/**
+ * hackathon date
+ */
+export const hachathonDate = new Date(2017, 10, 28);
+
+export const startTiming = {
+	hour: '11',
+	minute: '00',
+};
+
+export const deadline = {
+	hour: '17',
+	minute: '00',
+};
+
+export default 'CONFIG';
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 import '../css/index.css';
+import { hachathonDate, startTiming } from './appConfig';
+
+const nowTimeSeconds = parseInt(new Date().getTime() / 1000, 10);
+
+const start = hachathonDate;
+start.setHours(parseInt(startTiming.hour, 10));
+start.setMinutes(parseInt(startTiming.minute, 10));
+start.setSeconds(0, 0);
+
+const startTimeSeconds = parseInt(start.getTime() / 1000, 10);
+let timeDistance = startTimeSeconds - nowTimeSeconds;
+
+const leftDays = Math.floor(timeDistance / 60 / 60 / 24);
+timeDistance -= leftDays * 24 * 60 * 60;
+
+const leftHours = Math.floor(timeDistance / 60 / 60);
+timeDistance -= leftHours * 60 * 60;
+
+const leftMinutes = Math.floor(timeDistance / 60);
+timeDistance -= leftMinutes * 60;
+
+console.log(`Days: ${leftDays} / Hours: ${leftHours} / Minutes: ${leftMinutes} / Seconds: ${timeDistance}`);
