3c2359565dc9a6796d204b7033e724748b4a0e80
Eddie Wen <eddiewen.tw@gmail.com>
2017-11-23 23:43:58 +0800

    perf: calculate the appropriate text number

--- src/js/appConfig.js
+++ src/js/appConfig.js
@@ -14,8 +14,24 @@ deadlineTiming.setMinutes(0);
 deadlineTiming.setSeconds(0, 0);
 
 /* eslint-disable max-len */
-export const catTypeing = 'aljjjgaaaaaaaaaaaaaaaaaaaaaaam,32;iop9gy7uk/lm;\\\\\\\\\\\\\\\\\\\\\\\\\\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayyyyyyyy................s@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@iooooooooooooooooooooooooooooooooooooooifffffffffffffffffffffffffffffff';
+const defaultTypingText = 'aljjjgaaaaaaaaaaaaaaaaaaaaaaam,32;iop9gy7uk/lm;\\\\\\\\\\\\\\\\\\\\\\\\\\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayyyyyyyy................s@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ioooooooooooooooooooooooooooooooooooooifffffffffffffffffffffffffffffffo23orl;wef;p34io3m23ihcdusihcowefdfnbdjewd';
 /* eslint-enable max-len */
+
+const textWidth = 48;
+const textHeight = 92;
+
+const totalTextNumber = Math.floor(window.innerWidth / textWidth) *
+	(
+		window.innerHeight % textHeight === 0 ?
+			window.innerHeight / textHeight
+			:
+			Math.floor(window.innerHeight / textHeight) + 1
+	);
+
+export const catTyping = totalTextNumber < defaultTypingText.length ?
+	defaultTypingText.substr(0, totalTextNumber)
+	:
+	`${defaultTypingText}${'y'.repeat(totalTextNumber - defaultTypingText.length)}`;
 
 export const timer = {
 	days: 0,
